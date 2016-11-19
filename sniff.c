0562652b920977d7d149c5f1bc44c1090ba032eb
Christopher Leonard <chris.j.leonard@gmx.com>
2016-11-19 05:02:17 +0000

    minor

--- sniff.c
+++ sniff.c
@@ -26,8 +26,7 @@ static void dump_hex(const unsigned char *payload, size_t data_bytes)
   static const size_t output_sz = 20; // Output this many bytes at a time
   size_t i;
   while (data_bytes > 0) {
-    const size_t output_bytes =
-      data_bytes < output_sz ? data_bytes : output_sz;
+    const size_t output_bytes = data_bytes < output_sz ? data_bytes : output_sz;
     // Print data in raw hexadecimal form
     for (i = 0; i < output_sz; ++i) {
       if (i < output_bytes) {
