8dc8e3aae7ddeb6d1e47fcaa9be65ace9578b7b0
Christopher Leonard <chris.j.leonard@gmx.com>
2016-11-19 04:59:00 +0000

    sniff.c: Refactoring. Split code up

--- sniff.c
+++ sniff.c
@@ -9,61 +9,66 @@
 
 #include "dispatch.h"
 
+static void dump_mac_address(const u_char *const addr)
+{
+  size_t i;
+  for (i = 0; i < 6; ++i) {
+    printf("%02x", addr[i]);
+    if (i < 5) {
+      putchar(':');
+    }
+  }
+  putchar('\n');
+}
+
+static void dump_hex(const unsigned char *payload, size_t data_bytes)
+{
+  static const size_t output_sz = 20; // Output this many bytes at a time
+  size_t i;
+  while (data_bytes > 0) {
+    const size_t output_bytes =
+      data_bytes < output_sz ? data_bytes : output_sz;
+    // Print data in raw hexadecimal form
+    for (i = 0; i < output_sz; ++i) {
+      if (i < output_bytes) {
+        printf("%02x ", payload[i]);
+      } else {
+        printf ("   "); // Maintain padding for partial lines
+      }
+    }
+    printf ("| ");
+    // Print data in ascii form
+    for (i = 0; i < output_bytes; ++i) {
+      const char byte = payload[i];
+      if (isprint(byte)) {
+        printf("%c", byte);
+      } else {
+        printf(".");
+      }
+    }
+    printf("\n");
+    payload += output_bytes;
+    data_bytes -= output_bytes;
+  }
+}
+
 // Utility/Debugging method for dumping raw packet data
 static void dump(const unsigned char *const data, const size_t length) {
-  size_t i;
   static size_t pcount = 0;
   // Decode Packet Header
   const struct ether_header *const eth_header = (const void *)data;
-  printf("\n\n === PACKET %zu HEADER ===", pcount);
+  printf(" === PACKET %zu HEADER ===", pcount);
   printf("\nSource MAC: ");
-  for (i = 0; i < 6; ++i) {
-    printf("%02x", eth_header->ether_shost[i]);
-    if (i < 5) {
-      printf(":");
-    }
-  }
+  dump_mac_address(eth_header->ether_shost);
   printf("\nDestination MAC: ");
-  for (i = 0; i < 6; ++i) {
-    printf("%02x", eth_header->ether_dhost[i]);
-    if (i < 5) {
-      printf(":");
-    }
-  }
+  dump_mac_address(eth_header->ether_dhost);
   printf("\nType: %hu\n", eth_header->ether_type);
   printf(" === PACKET %zu DATA ==\n", pcount);
   // Decode Packet Data (Skipping over the header)
   if (length > ETH_HLEN) {
-    size_t data_bytes = length - ETH_HLEN;
-    const unsigned char *payload = data + ETH_HLEN;
-    static const size_t output_sz = 20; // Output this many bytes at a time
-    while (data_bytes > 0) {
-      const size_t output_bytes =
-        data_bytes < output_sz ? data_bytes : output_sz;
-      // Print data in raw hexadecimal form
-      for (i = 0; i < output_sz; ++i) {
-        if (i < output_bytes) {
-          printf("%02x ", payload[i]);
-        } else {
-          printf ("   "); // Maintain padding for partial lines
-        }
-      }
-      printf ("| ");
-      // Print data in ascii form
-      for (i = 0; i < output_bytes; ++i) {
-        const char byte = payload[i];
-        if (isprint(byte)) {
-          // Byte is in printable ascii range
-          printf("%c", byte);
-        } else {
-          printf(".");
-        }
-      }
-      printf("\n");
-      payload += output_bytes;
-      data_bytes -= output_bytes;
-    }
+    dump_hex(data + ETH_HLEN, length - ETH_HLEN);
   }
+  printf("\n\n");
   pcount++;
 }
 
@@ -76,7 +81,7 @@ void sniff(const char *const interface, const int verbose) {
     fprintf(stderr, "Unable to open interface %s\n", errbuf);
     exit(EXIT_FAILURE);
   }
-  printf("SUCCESS! Opened %s for capture\n", interface);
+  printf("SUCCESS! Opened %s for capture\n\n\n", interface);
   // Capture packets (very ugly code)
   while (1) {
     // Capture a packet
